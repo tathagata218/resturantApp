e8c42633a73a4379e53dc8d18a9d2a7aa3b38c9c
Tdas <tathagata218@yahoo.com>
2018-07-09 14:28:52 -0500

    This is First Part Code

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+const bodyParser = require('body-parser')
+const path = require('path')
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.get('/',( req,res ) => {
+    res.sendFile(path.join(__dirname, "client/index.html"));
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`You are Lisining to PORT ${PORT}`)
+})
+
+
+
+
+
