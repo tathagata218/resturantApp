5b506be7ab19a350c9b78c00ad7374991e9d6525
Tdas <tathagata218@yahoo.com>
2018-07-09 14:50:50 -0500

    This is Food Hung

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,17 @@
-console.log('you are in JS')
+var phonecatApp = angular.module('FoodHung', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser')
 const path = require('path')
 
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('client'));
 
 app.get('/',( req,res ) => {
     res.sendFile(path.join(__dirname, "client/index.html"));
