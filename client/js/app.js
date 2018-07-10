ff2955867c3f77b52f0fadbb4b5f0bf375227885
Tdas <tathagata218@yahoo.com>
2018-07-10 16:02:37 -0500

    This is updated

--- client/js/app.js
+++ client/js/app.js
@@ -2,16 +2,10 @@ var phonecatApp = angular.module('FoodHung', []);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
 phonecatApp.controller('bookResults', function PhoneListController($scope) {
+  
+  
+  
   $scope.phones = [
-    {
-      name: 'Nexus S',
-      snippet: 'Fast just got faster with Nexus S.'
-    }, {
-      name: 'Motorola XOOM™ with Wi-Fi',
-      snippet: 'The Next, Next Generation tablet.'
-    }, {
-      name: 'MOTOROLA XOOM™',
-      snippet: 'The Next, Next Generation tablet.'
-    }
+    
   ];
 });
