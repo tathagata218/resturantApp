b242ec1dab1028cff094ee5d2a1db10fcc6a9065
Tdas <tathagata218@yahoo.com>
2018-07-10 16:19:23 -0500

    This is updated

--- client/js/app.js
+++ client/js/app.js
@@ -3,7 +3,9 @@ var phonecatApp = angular.module('FoodHung', []);
 // Define the `PhoneListController` controller on the `phonecatApp` module
 phonecatApp.controller('bookResults', function PhoneListController($scope) {
   
-  
+  $scope.submit = function () { 
+      console.log($scope.text)
+   }
   
   $scope.phones = [
     
