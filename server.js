9f8b0526e5487ab1b11c466e32490850bd507fa9
Tdas <tathagata218@yahoo.com>
2018-07-12 14:36:12 -0500

    This is Updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = 8080
 const bodyParser = require('body-parser')
 const path = require('path')
-
+const data = [];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -16,6 +16,9 @@ app.get('/',( req,res ) => {
 
 app.post('/data',(req,res)=> {
     console.log(req.body)
+    if(req.body) { 
+        data.push(req.body)
+    }
     res.status(200)
 })
 
