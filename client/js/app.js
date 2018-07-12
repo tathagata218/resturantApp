dc5d300d344bfab152a4e12594a73b1e9fdef5f0
Tdas <tathagata218@yahoo.com>
2018-07-12 14:10:45 -0500

    This is updated

--- client/js/app.js
+++ client/js/app.js
@@ -1,14 +1,12 @@
 var phonecatApp = angular.module('FoodHung', []);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
-phonecatApp.controller('resturantApp', function resturantApp($scope) {
+phonecatApp.controller('resturantApp', function resturantApp($scope,$http) {
   $scope.value = []
   $scope.submit = function () { 
       console.log($scope.inputText)
       let data = $scope.inputText
-      let config = headers: {
-        'Content-Type': 'data'
-      }
+    
     $http.post('/data',data).then(()=>{console.log('it Worked')},()=>{'sorry'})
     }
   
