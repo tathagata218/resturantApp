4e3c6dd6fc46aafdc4e6bee8b93310e5d6894034
Tdas <tathagata218@yahoo.com>
2018-07-13 18:55:54 -0500

    This is updated

--- client/js/app.js
+++ client/js/app.js
@@ -3,15 +3,13 @@ var phonecatApp = angular.module('FoodHung', []);
 // Define the `PhoneListController` controller on the `phonecatApp` module
 phonecatApp.controller('resturantApp', function resturantApp($scope,$http) {
   $scope.value = []
-  $scope.submit = function () { 
-      console.log($scope.inputText)
-      $http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-      $http.defaults.headers.common.Authorization = 'kpKQNlt26wcMq4d4ZEDISu4NHXumEvl5KxrgsZnx4rU3UgMNd47fFIZo7n_Dz1gA8cO7bD4TpwOcSZrAK1bxAKEeVhPgO1zpPTiL1HMmRchQwpAd7zb62BG11bhHW3Yx';
-      let url = 'http://api.yelp.com/v3/businesses/search'
-      
-    $http.get(url).then((data)=>console.log(data))
-   
+  $scope.submit = function () {
+    let test = 'delis';
+    console.log(test)
+    
+     
     }
+
   $scope.clear = function (param) {
     $http.post('/data',data)
     }
