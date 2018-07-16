52c1235b7afd5645b3b12517a0fec2a6a948943e
Tdas <tathagata218@yahoo.com>
2018-07-15 23:09:14 -0500

    this is updated

--- client/js/app.js
+++ client/js/app.js
@@ -5,7 +5,7 @@ phonecatApp.controller('resturantApp', function resturantApp($scope,$http) {
   
   let lat = null
   let long = null
-  
+  let mymap = null
   function getLocation () {
     if(navigator.geolocation)
     { 
@@ -17,10 +17,11 @@ phonecatApp.controller('resturantApp', function resturantApp($scope,$http) {
   
   function Position(pos){
     lat = pos.coords.latitude
-    long = pos.coords.longitude 
+    long = pos.coords.longitude
+    mymap = L.map('mapid').setView([lat, long], 13); 
   }
   
-  let mymap = L.map('mapid').setView([lat, long], 13);
+  
 
   $scope.submit = function () {
     let test = {data : $scope.inputText,
@@ -28,18 +29,20 @@ phonecatApp.controller('resturantApp', function resturantApp($scope,$http) {
                 lat : lat};
     console.log(test)
     $http.post('/data',test).success((data)=>{
-      
+      console.log(data)
+      let info = JSON.parse(data.data)
       
       L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${data.leaflet}`, {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       id: 'mapbox.streets',
-      accessToken: leaflet
+      accessToken: data.leaflet
           }).addTo(mymap);
 
 
       for(let i = 0; i < data.bussiness; i++) {
-        var marker = L.marker([data.bussiness[i].coordinates.latitude, data.bussiness[i].coordinates.longitude]).addTo(mymap);
+        
+         L.marker([info.bussiness[i].coordinates.latitude, info.bussiness[i].coordinates.longitude]).addTo(mymap);
 
         
       
