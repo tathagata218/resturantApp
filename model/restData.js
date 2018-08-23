37c4e42d073e48b5b824cffe9cab80143c72018d
Tdas <tathagata218@yahoo.com>
2018-08-23 14:38:49 -0500

    This is udpated

--- model/restData.js
+++ model/restData.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const restData = new Schema({
-    name : {type : String, require: true},
-    info : {type : String, require: true}
+    resturantName : {type : String, require: true},
+    resturantAddress : {type : String, require: true},
+    resturantPhone : {type : String, require: true}
 }) 
 
 const resturantData = mongoose.model('resturantData',restData)
