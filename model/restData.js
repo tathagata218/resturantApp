3d67e2564bb023eba7aa5dbd57fceb3e758cd94f
Tdas <tathagata218@yahoo.com>
2018-08-23 14:36:08 -0500

    This is updated

--- model/restData.js
+++ model/restData.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.schema
+const Schema = mongoose.Schema
 
 const restData = new Schema({
     name : {type : String, require: true},
     info : {type : String, require: true}
 }) 
 
-const resturantData = mongoose.model('resturantData','restData')
+const resturantData = mongoose.model('resturantData',restData)
 
 module.exports = resturantData
