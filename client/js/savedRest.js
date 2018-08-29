fc0afc4d94c093baa6074a8952960f8525cbf9a1
Tathagata Das <tathagata218@yahoo.com>
2018-08-29 14:17:35 -0500

    This is updated

--- client/js/savedRest.js
+++ client/js/savedRest.js
@@ -7,6 +7,7 @@ secondPart.controller('displayResturants', function displayResturants($scope,$ht
     
 console.log('You are working here')
 $http.get('/allData').success((data)=>{
+    $scope.allArrData = data
     $scope.test = data.data
     console.log(data)
 })
