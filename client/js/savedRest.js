3f1677588b4fb2f739b9ced4a525d48b9a5f8c84
Tathagata Das <tathagata218@yahoo.com>
2018-08-30 13:19:34 -0500

    This is updated

--- client/js/savedRest.js
+++ client/js/savedRest.js
@@ -4,11 +4,18 @@ const secondPart = angular.module('savedPart',[]);
 
 
 secondPart.controller('displayResturants', function displayResturants($scope,$http) {
-    
-console.log('You are working here')
+
 $http.get('/allData').success((data)=>{
     $scope.allArrData = data.data
     //$scope.test = data.data
     console.log(data.data)
 })
+
+
+$scope.delRest = function ($event) {
+    let dataID = $event.currentTarget.attributes[2].nodeValue
+    console.log(dataID)
+
+}
+
 })
