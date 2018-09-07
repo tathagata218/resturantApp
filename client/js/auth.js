b30e740f9d16378c98ae767f76780d494b0d9243
Tathagata Das <tathagata218@yahoo.com>
2018-09-07 14:56:56 -0500

    This is updated

--- client/js/auth.js
+++ client/js/auth.js
@@ -4,7 +4,22 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault()
         let email = document.getElementById("emailInp").value
         let password = document.getElementById("passInp").value
-        
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", '/login', true);
+
+        //Send the proper header information along with the request
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        xhr.onreadystatechange = function() {//Call a function when the state changes.
+            if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                console.log('The data Has been sent')
+            }
+        }
+        xhr.send("foo=bar&lorem=ipsum"); 
+
+
+
         console.log(email,password)
     })
     
