adac7a025b7e6449861008fcc9f517329703bda8
Tathagata Das <tathagata218@yahoo.com>
2018-09-09 10:21:30 -0500

    THis is updated

--- client/js/auth.js
+++ client/js/auth.js
@@ -1,43 +1,10 @@
 const authorization = angular.module('authPage',[])
 
 
+authorization.controller('authInfo', function resturantApp($scope,$http) {
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Dom is loaded')
-    document.getElementById('signinbutton').addEventListener("click",function(){
-        event.preventDefault()
-        let email = document.getElementById("emailInp").value
-        let password = document.getElementById("passInp").value
-        let data = {
-            email : email,
-            pass : password
-        }
-
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", '/auth', true);
-
-        //Send the proper header information along with the request
-        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
-        xhr.onreadystatechange = function() {//Call a function when the state changes.
-            //
-            if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-                console.log('The data Has been sent')
-            }
-        }
-        xhr.send( JSON.stringify(data));
+});
 
 
-
-
-        
-    })
-    
-    
-    
-
-    
-    // your code here
- }, true);
